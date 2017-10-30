d9af4fb09e442f98b63d1ef09cb433e329b716f3
rrsilaya <rrsilaya@up.edu.ph>
2017-10-30 22:20:56 +0800

    Modify database schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,114 +1,133 @@
 -- User
-DROP USER IF EXISTS 'bluetag'@'localhost';
-CREATE USER 'bluetag'@'localhost' IDENTIFIED BY 'bluetagadmin';
+DROP USER IF EXISTS 'bt_default'@'localhost';
+DROP USER IF EXISTS 'bt_master'@'localhost';
+DROP USER IF EXISTS 'bt_employee'@'localhost';
+CREATE USER 'bt_default'@'localhost' IDENTIFIED BY 'default';
+CREATE USER 'bt_master'@'localhost' IDENTIFIED BY 'master';
+CREATE USER 'bt_employee'@'localhost' IDENTIFIED BY 'employee';
 
 -- Database
-DROP DATABASE IF EXISTS apparel;
-CREATE DATABASE apparel;
+DROP DATABASE IF EXISTS bluetag;
+CREATE DATABASE bluetag;
 
--- Grant Privileges
-GRANT SUPER ON *.* TO 'bluetag'@'localhost';
-GRANT ALL PRIVILEGES ON apparel.* TO 'bluetag'@'localhost';
+USE bluetag;
 
-USE apparel;
-
+-- Database Definition
 CREATE TABLE employee (
-	username VARCHAR(10) NOT NULL,
-  password VARCHAR(24) NOT NULL, -- to be adjusted (for encyrption)
-  type ENUM(
+  `id` INT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(10) NOT NULL,
+  `password` VARCHAR(24) NOT NULL, -- to be adjusted (for encyrption)
+  `type` ENUM(
     'manager',
     'employee'
   ) DEFAULT 'employee' NOT NULL,
 
 	CONSTRAINT employee_username_pk
-    PRIMARY KEY(username),
+    PRIMARY KEY(`id`),
   CONSTRAINT employee_username_uk
-    UNIQUE KEY(username)
+    UNIQUE KEY(`username`)
 );
 
-CREATE TABLE audit (
-  id VARCHAR(16) NOT NULL,
-  timestamp TIMESTAMP NOT NULL,
-  employee VARCHAR(10) NOT NULL,
-  action ENUM(
-    'view',
-    'add',
-    'sell',
-    'remove',
-    'modify'
-  ) DEFAULT 'view' NOT NULL,
+CREATE TABLE log (
+  `id` VARCHAR(16) NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL,
+  `action` VARCHAR(30) NOT NULL,
+  `employee` VARCHAR(10) NOT NULL,
 
-  CONSTRAINT audit_id_pk
-    PRIMARY KEY(id),
-  CONSTRAINT audit_employee_fk
-    FOREIGN KEY(employee)
-    REFERENCES employee(username)
+  CONSTRAINT `audit_id_pk`
+    PRIMARY KEY(`id`),
+  CONSTRAINT `audit_employee_fk`
+    FOREIGN KEY(`employee`)
+    REFERENCES employee(`username`)
 );
 
 CREATE TABLE orderRequest (
-  id VARCHAR(16) NOT NULL,
-  timestamp TIMESTAMP NOT NULL,
-  status ENUM(
+  `id` VARCHAR(16) NOT NULL,
+  `timestamp` TIMESTAMP NOT NULL,
+  `status` ENUM(
     'pending',
     'delivered',
     'cancelled'
   ) DEFAULT 'pending' NOT NULL,
+  `employee` VARCHAR(10) NOT NULL,
 
-  CONSTRAINT orderRequest_id_pk
-    PRIMARY KEY(id)
+  CONSTRAINT `orderRequest_id_pk`
+    PRIMARY KEY(`id`),
+  CONSTRAINT `orderRequest_employee_fk`
+    FOREIGN KEY(`employee`)
+    REFERENCES employee(`username`)
 );
 
 CREATE TABLE orderRequestItem (
-  id INT NOT NULL AUTO_INCREMENT,
-  item VARCHAR(30) NOT NULL,
-  qty INT DEFAULT 1 NOT NULL,
-  request VARCHAR(16) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `item` VARCHAR(30) NOT NULL,
+  `qty` INT DEFAULT 1 NOT NULL,
+  `request` VARCHAR(16) NOT NULL,
 
-  CONSTRAINT orderRequestItem_id_pk
-    PRIMARY KEY(id),
-  CONSTRAINT orderRequestItem_id_fk
-    FOREIGN KEY(request)
-    REFERENCES orderRequest(id)
+  CONSTRAINT `orderRequestItem_id_pk`
+    PRIMARY KEY(`id`),
+  CONSTRAINT `orderRequestItem_id_fk`
+    FOREIGN KEY(`request`)
+    REFERENCES orderRequest(`id`)
 );
 
 CREATE TABLE apparel (
-  id VARCHAR(16) NOT NULL,
-  type VARCHAR(30) NOT NULL,
-  size VARCHAR(10) NOT NULL,
-  color VARCHAR(20),
-  qty INT DEFAULT 1 NOT NULL,
-  price DECIMAL NOT NULL,
-  deliveryDate TIMESTAMP NOT NULL,
+  `id` VARCHAR(16) NOT NULL,
+  `type` VARCHAR(30) NOT NULL,
+  `size` VARCHAR(10) NOT NULL,
+  `color` VARCHAR(20),
+  `qty` INT DEFAULT 1 NOT NULL,
+  `price` DECIMAL NOT NULL,
+  `deliveryDate` TIMESTAMP NOT NULL,
+  `employee` VARCHAR(10) NOT NULL,
 
-  CONSTRAINT apparel_id_pk
-    PRIMARY KEY(id)
+  CONSTRAINT `apparel_id_pk`
+    PRIMARY KEY(`id`),
+  CONSTRAINT `apparel_employee_fk`
+    FOREIGN KEY(`employee`)
+    REFERENCES employee(`username`)
 );
 
 CREATE TABLE discount (
-  id INT NOT NULL AUTO_INCREMENT,
-  date DATE NOT NULL,
-  rate INT NOT NULL,
-  isActive BOOLEAN DEFAULT true NOT NULL,
-  apparel VARCHAR(16) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `date` DATE NOT NULL,
+  `rate` INT NOT NULL,
+  `isActive` BOOLEAN DEFAULT true NOT NULL,
+  `apparel` VARCHAR(16) NOT NULL,
+  `employee` VARCHAR(10) NOT NULL,
 
-  CONSTRAINT discount_id_pk
-    PRIMARY KEY(id),
-  CONSTRAINT discount_apparel_fk
-    FOREIGN KEY(apparel)
-    REFERENCES apparel(id)
+  CONSTRAINT `discount_id_pk`
+    PRIMARY KEY(`id`),
+  CONSTRAINT `discount_apparel_fk`
+    FOREIGN KEY(`apparel`)
+    REFERENCES apparel(`id`),
+  CONSTRAINT `discount_employee_fk`
+    FOREIGN KEY(`employee`)
+    REFERENCES employee(`username`)
 );
 
 CREATE TABLE sale (
-  id INT NOT NULL AUTO_INCREMENT,
-  date DATE NOT NULL,
-  qty INT DEFAULT 1 NOT NULL,
-  apparel VARCHAR(16) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `date` DATE NOT NULL,
+  `qty` INT DEFAULT 1 NOT NULL,
+  `apparel` VARCHAR(16) NOT NULL,
+  `employee` VARCHAR(10) NOT NULL,
 
-  CONSTRAINT sale_id_pk
-    PRIMARY KEY(id),
-  CONSTRAINT sale_aparel_pk
-    FOREIGN KEY(apparel)
-    REFERENCES apparel(id)
+  CONSTRAINT `sale_id_pk`
+    PRIMARY KEY(`id`),
+  CONSTRAINT `sale_aparel_pk`
+    FOREIGN KEY(`apparel`)
+    REFERENCES apparel(`id`),
+  CONSTRAINT `sale_employee_fk`
+    FOREIGN KEY(`employee`)
+    REFERENCES employee(`username`)
 );
 
-INSERT INTO employee VALUES ('admin', 'admin', 'manager');
+-- Privileges
+GRANT SELECT ON bluetag.employee TO 'bt_default'@'localhost';
+GRANT ALL PRIVILEGES ON bluetag.* TO 'bt_master'@'localhost';
+GRANT INSERT, SELECT, DELETE ON bluetag.orderRequest TO 'bt_employee'@'localhost';
+GRANT INSERT, SELECT, DELETE ON bluetag.orderRequestItem TO 'bt_employee'@'localhost';
+GRANT INSERT, SELECT, DELETE ON bluetag.apparel TO 'bt_employee'@'localhost';
+GRANT INSERT, SELECT, DELETE ON bluetag.discount TO 'bt_employee'@'localhost';
+GRANT INSERT, SELECT, DELETE ON bluetag.sale TO 'bt_employee'@'localhost';
